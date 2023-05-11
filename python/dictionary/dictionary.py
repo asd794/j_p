07369953720db20187f1e905ce516b9eff34e2a9
jay.hsu <jay.hsu@example.com>
2023-05-11 17:03:13 +0800

    update python

---
+++ python/dictionary/dictionary.py
@@ -0,0 +1,7 @@
+s1={3,4,5}
+print(2 not in s1)
+s2={4,5,6,7}
+s3=s1&s2 #交集
+print(s3)
+s3=s1|s2 #聯集,不重複取
+print(s3)
--- python/test1/test1.py
+++
@@ -1,8 +0,0 @@
-a=1
-b=2
-c=True
-d="hello world"
-e='hello world'
-f=[1,2,3]
-g=[4,5,6]
-print(f+g)
