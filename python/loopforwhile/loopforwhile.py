3968a5ab248ad3c03930e8e05401f960068559cd
jay.hsu <jay.hsu@example.com>
2023-05-11 19:00:32 +0800

    update python

---
+++ python/loopforwhile/loopforwhile.py
@@ -0,0 +1,15 @@
+# n=1
+# sum=0
+# while n<=10:
+#     print(n)
+#     sum+=n
+#     n+=1
+# print(sum)
+for i in [3,5,1]:
+    print(i)
+
+for i in "[3,5,1]\"":
+    print(i)
+
+for i in range(5):
+    print(i)
