0d0ad9dd1ae8f5310a645dba05cefef9d7eb450d
jay.hsu <jay.hsu@example.com>
2023-05-12 18:36:50 +0800

    update

--- python/loopforwhile/loopforwhile.py
+++ python/loopforwhile/loopforwhile.py
@@ -5,6 +5,11 @@
 #     sum+=n
 #     n+=1
 # print(sum)
+sum=0
+for i in range(1,11):
+    sum+=i
+print(sum)
+
 for i in [3,5,1]:
     print(i)
 
@@ -13,3 +18,20 @@ for i in "[3,5,1]\"":
 
 for i in range(5):
     print(i)
+
+for i in range(2):
+    print(i)
+
+
+y=0
+long=len("Hello")
+array=["none"]*long
+for i in "Hello":
+    if i in array:
+        y+=1
+        continue
+    array[y]=i
+    y+=1
+
+print(array)
+
