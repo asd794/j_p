5b821037a1dfa88b2db302a0629559134b353b61
jay.hsu <jay.hsu@example.com>
2023-05-18 18:41:06 +0800

    update

--- python/function/function.py
+++ python/function/function.py
@@ -1,5 +1,5 @@
 def add (n1,n2):
-    sum=n1+n2
+    sum=n1/n2
     print(sum)
     return sum
 
@@ -9,10 +9,17 @@ def funfor(max):
         sum+=i
     return sum
 
-value=add(55,99)
+def say(*msgs):
+    for i in msgs:
+        print(i)
+    return "緊張囉"
+
+value=add(2,1)
 print(value*2)
+add(n2=2,n1=1)
 
 sum=funfor(10)+funfor(11)
 print(sum)
 
+print(say("說話",222,"HI"))
 
