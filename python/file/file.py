2d460f567191df62494962119093f639f5d37680
jay.hsu <jay.hsu@example.com>
2023-05-22 18:41:31 +0800

    老哥我修好了

--- python/file/file.py
+++ python/file/file.py
@@ -3,20 +3,19 @@
 # file.close
 
 with open("data.txt",mode="w",encoding="utf-8") as file:
-    file.write("1\n2\n3")
+    file.write("1\n19\n30")
 
 array=[]
-index=0
-
+sum=0
 with open("data.txt",mode="r",encoding="utf-8") as file:
-    data=file.read()
+    # data=file.read()
+    # print(data)
     for line in file:
         # print(file.readlines())
         # print(array[0])
+        sum+=int(line)
         print(line)
         array.append(line)
 
-        # print(line)
-
-
 print(array)
+print(sum)
