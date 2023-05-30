96c13ef3e11c14f4e7b3a21011141d6990a9c060
jay.hsu <jay.hsu@example.com>
2023-05-30 11:25:11 +0800

    update

--- python/crawler/crawler-test.py
+++ python/crawler/crawler-test.py
@@ -21,22 +21,26 @@ date_array=[]
 author_array=[]
 titles_array=[]
 ppt_article_url_array=[]
+content_array=[]
 
 x=0
 for i in date:
-    date_array.append(i.string) # class裡面的String值加入date_array陣列
+    date_array.append(i.string) # class=date裡面的String值加入date_array陣列
     # print(date_array[x])
     x+=1
 y=0
 for i in author:
-    author_array.append(i.string) # class裡面的String值加入author_array陣列
+    author_array.append(i.string) # class=author裡面的String值加入author_array陣列
     # print(author_array[y])
     y+=1
 z=0
 for i in titles:
-    titles_array.append(i.a.string) # class裡面的String值加入1titles_array陣列
+    titles_array.append(i.a.string) # class=titles裡面的String值加入1titles_array陣列
     href_url=root.find("a",string=i.a.string)   # 用文章標題(i.a.string)去搜尋對應的class元素
-    ppt_article_url_array.append(ppt_url+href_url['href'])   # href_url['href'] 加上['href']搜尋底下的href
+    if '(本文已被刪除)' in titles:  # 判斷文章是否被刪除
+        ppt_article_url_array.append('(本文已被刪除)')
+    else:
+        ppt_article_url_array.append(ppt_url+href_url['href'])   # href_url['href'] 加上['href']搜尋底下的href
     # print(titles_array[z])
     z+=1
 
@@ -45,37 +49,37 @@ for i in ppt_article_url_array:
     print(date_array[xx],author_array[xx],titles_array[xx],i)
     xx+=1
 
-print("----------------")
-# --------------內文
-url2="https://www.ptt.cc/bbs/Gossiping/M.1685093096.A.D51.html"
-request2=req.Request(url2,headers={
-    "cookie":"over18=1",
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-})
-with req.urlopen(request2) as response:
-    data2=response.read().decode("utf-8")
-root2=bs4.BeautifulSoup(data2,"html.parser")
-maincontent=root2.find("div",id="main-content") # 主要內容main-content
+print("----------------")   # --------------------------------內文
 
-fromptt=root2.find("span",class_="f2")
-fromptt2=root2.find_all("span",class_="article-meta-value")
+yy=0
+for i in ppt_article_url_array:
 
+    url2=i
+    request2=req.Request(url2,headers={
+        "cookie":"over18=1",
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+    })
+    with req.urlopen(request2) as response:
+        data2=response.read().decode("utf-8")
+    root2=bs4.BeautifulSoup(data2,"html.parser")
+    maincontent=root2.find("div",id="main-content") # 主要內容main-content
+    fromptt=root2.find("span",class_="f2")
+    fromptt2=root2.find_all("span",class_="article-meta-value") # 尋找class=article-meta-value 位置放入fromptt2陣列 ,總共有4筆一樣的class
 
-print(len(str(fromptt2[3])+"</div>")) # 前面文字的長度
-print(str(maincontent).find(str(fromptt2[3])+"</div>")) # 前面文字的位置
-# print(fromptt)
-# print(article_meta_value)
+    n1=len(str(fromptt2[3])+"</div>") # 前面文字的長度 ,發文時間在fromptt2第四筆陣列加上</div> ,得以算出長度 
+    # print(n1)
+    n2=str(maincontent).find(str(fromptt2[3])+"</div>") # 前面文字的位置 ,發文時間在fromptt2第四筆陣列加上</div> ,得以算出位置(結果會是頭的位置)
+    # print(n2)
+    # n3=len(str(maincontent)) # main-content總長度
+    # print(n3)
+    n4=str(maincontent).find('<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc),')    # 先找文章後面的'<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc),'在main-content的位置  <-- 固定的字串
+    # print(n4)
+    content=str(maincontent)[n1+n2:n4-1]
+    content_array.append(content)
+    # print(content)
 
-# if "※ 發信站: 批踢踢實業坊(ptt.cc)" in str(fromptt):
-#     print("有")
-# else:
-#     print("無")
+    # print(content_array[yy])
+    yy+=1
 
-# ttest="Hi , I am Jay. 你好阿"
-# print(ttest.find("Hi"))
-
-print(len(str(maincontent))) # main-content總長度
-
-# print(str(maincontent).find('<div class="article-metaline"><span class="article-meta-tag">時間</span><span class="article-meta-value">Fri May 26 17:24:54 2023</span></div>'))  # 先找文章前面的文字位置
-print(str(maincontent).find('<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc),'))    # 先找文章後面的'<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc),'在main-content的位置  <-- 固定的字串
-print(str(maincontent)[526+70:655-1])
+print(content_array[13])
+    
