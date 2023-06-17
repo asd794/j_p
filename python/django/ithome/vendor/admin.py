3de27c8d754234010805338492d12099731b0a6a
jay.hsu <jay.hsu@example.com>
2023-06-17 18:35:10 +0800

    更新django

---
+++ python/django/ithome/vendor/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Vendor, Food, VendorAdmin
+
+# admin.site.register(Vendor,VendorAdmin) # models.py
+# admin.site.register(Food) # models.py
---
+++ python/django/ithome/vendor/models.py
@@ -0,0 +1,46 @@
+from typing import Any, List, Tuple
+from django.db import models
+
+# Create your models here.
+class Vendor(models.Model):
+    vendor_name=models.CharField(max_length=20) # 攤販的名稱
+    store_name=models.CharField(max_length=10) # 攤販店家的名稱
+    phone_number=models.CharField(max_length=20) # 攤販的電話號碼
+    address=models.CharField(max_length=100) # 攤販的地址
+    
+    def __str__(self):
+        return self.vendor_name
+
+
+class Food(models.Model):
+    food_name = models.CharField(max_length=30) # 食物名稱
+    price_name=models.DecimalField(max_digits=3,decimal_places=0) # 食物價錢
+    food_vendor=models.ForeignKey(Vendor, on_delete=models.CASCADE) # 代表這食物是由哪一個攤販所做的
+
+    def __str__(self):
+        return self.food_name
+
+from django.contrib import admin
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'vendor_name', 'store_name', 'phone_number', 'address']
+
+from django.utils.translation import gettext_lazy as _
+class Morethanfifty(admin.SimpleListFilter):
+    title=_('price')
+    parameter_name='compareprice'
+    def lookups(self, request: Any, model_admin):
+        return (('>50',_('>50')),('<=50',_('<=50')),)
+    def queryset(self, request, queryset):
+        if self.value() == '>50':
+            return queryset.filter(price_name__gt=50)
+        if self.value() == '<=50':
+            return queryset.filter(price_name__lte=50)
+              
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Food._meta.fields]
+    # list_display = ['id', 'food_name', 'price_name', 'food_vendor'] # 等於上面那行
+    list_filter=(Morethanfifty,) # 過濾 price_name
+
